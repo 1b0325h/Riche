b534390e244f5c886f68d1e280cbf6bb42f8d4aa
1b0325h <melano@1b0325h.p001.jp>
2021-02-23 10:39:18 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+mode = 0
 
-def color_mode():
-   value = 0
-   if value:
-      return 1 #dark
-   return 0 #light
+def render(mode):
+   return render_template("index.html", mode=mode)
 
-def ddg(keywords):
-   return "https://duckduckgo.com/&q=" + str(keywords)
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-   return render_template("index.html", title="Riche",
-      color_mode=color_mode())
-
+   global mode
+   if request.method == "POST":
+      if int(request.form["value"]):
+         mode = 1
+      else:
+         mode = 0
+      return render(mode)
+   else:
+      return render(mode)
+   
 @app.route("/results", methods=["POST"])
 def results():
-   if request.form["keywords"]:
-      return redirect(ddg(request.form["keywords"]))
-   return redirect("/")
+   if request.form["keyword"]:
+      return redirect("https://duckduckgo.com/&q="
+         + str(request.form["keyword"]))
+   else:
+      return redirect("/")
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+   app.run()
