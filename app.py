625fdd1ed18169a296858482dbd1951017278003
1b0325h <melano@1b0325h.p001.jp>
2021-10-16 11:19:40 +0900

    senju

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
+import time
 from unicodedata import east_asian_width
 import urllib.parse
-import time
 
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, redirect
 from flask_assets import Environment, Bundle
 from hamlish_jinja import HamlishTagExtension
-from bs4 import BeautifulSoup
 import requests
 
 
-app = Flask(__name__)
 
+SITE = "https://www.google.co.jp/search"
+HEADER = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebK"\
+          "it/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+
+
+
+app = Flask(__name__)
 mode = True
-site = "https://www.google.co.jp/search"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebK"\
-           "it/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
 
 # Hamlish settings
 app.jinja_env.add_extension(HamlishTagExtension)
@@ -29,97 +32,100 @@ sass = Bundle("sass/blank.sass", "sass/style.sass",
 assets.register("css_all", sass)
 
 
+
 def search(keyword):
-   """Scraping with bs4"""
+    def _logging(s):
+        cnvtime = time.strftime("%Y/%m/%d %H:%M:%S",
+                                time.strptime(time.ctime()))
+        print(f"[{cnvtime}] {s}")
 
-   def _logging(s):
-      cnvtime = time.strftime("%Y/%m/%d %H:%M:%S", time.strptime(time.ctime()))
-      print(f"[{cnvtime}] {s}")
+    def _replace(s):
+        return s.replace("\n", "").replace("\r", "")
 
-   def _replacement(s):
-      return s.replace("\n", "").replace("\r", "")
+    def _letters(s):
+        return sum(2 if east_asian_width(_) in "FWA" else 1 for _ in s)
 
-   def _letters(s):
-      return sum(2 if east_asian_width(_) in "FWA" else 1 for _ in s)
+    _logging(f"Search for '{keyword}'.")
 
-   _logging(f"Search for '{keyword}'.")
+    r = requests.get(SITE, params={"q": keyword}, headers=HEADER, timeout=7.0)
+    soup = BeautifulSoup(r.content, "html.parser")
 
-   r = requests.get(site, params={"q": keyword}, headers=headers, timeout=5.0)
-   soup = BeautifulSoup(r.content, "html.parser")
+    titles, links, urls, snippets = [[] for _ in range(4)]
 
-   titles, links, urls, snippets = [[] for _ in range(4)]
+    yurubf = soup.select(".yuRUbf > a")
+    for i in yurubf[:7]:
+        url = entry = contents = ""
 
-   yurubf = soup.select(".yuRUbf > a")
-   for i in yurubf[:7]:
-      url = entry = contents = ""
+        url = i.get("href").replace("/url?q=", "").split("&sa=U")[0]
+        url = urllib.parse.unquote(urllib.parse.unquote(url))
+        if "http://" in url or "https://" in url:
+            try:
+                entry = requests.get(url, headers=HEADER, timeout=7.0)
+                t = entry.elapsed.total_seconds()
+                _logging(f"- {t} *({url})")
+                entry.encoding = "utf-8"
+            except:
+                continue
+            links.append(url)
 
-      url = i.get("href").replace("/url?q=", "").split("&sa=U")[0]
-      url = urllib.parse.unquote(urllib.parse.unquote(url))
-      if "http://" in url or "https://" in url:
-         try:
-            entry = requests.get(url, headers=headers, timeout=5.0)
-            t = entry.elapsed.total_seconds()
-            _logging(f"- {t} *({url})")
-            entry.encoding = "utf-8"
-         except: continue
-         links.append(url)
+            contents = BeautifulSoup(entry.text, "html.parser")
 
-         contents = BeautifulSoup(entry.text, "html.parser")
-
-         if contents.select("title"):
-            titles.append(_replacement(contents.select("title")[0].get_text()))
-         else:
-            titles.append(url)
-
-         if contents.select("body"):
-            if _letters((body := contents.select("body")[0].get_text())) > 200:
-               snippets.append(body[:200] + "...")
+            if contents.select("title"):
+                titles.append(_replace(contents.select("title")[0].get_text()))
             else:
-               snippets.append(body)
-         else:
-            snippets.append("...")
+                titles.append(url)
 
-   for i in links:
-      if _letters(i) > 72:
-         urls.append(i[:42] + "...")
-      else:
-         urls.append(i)
+            if contents.select("body"):
+                if _letters(body := contents.select("body")[0].get_text()) > 200:
+                    snippets.append(body[:200] + "...")
+                else:
+                    snippets.append(body)
+            else:
+                snippets.append("...")
 
-   return (titles, links, urls, snippets)
+    for i in links:
+        if _letters(i) > 72:
+            urls.append(i[:42] + "...")
+        else:
+            urls.append(i)
+
+    return titles, links, urls, snippets
 
 
 def render(mode):
-   return render_template("index.haml", mode=int(mode))
+    return render_template("index.haml", mode=int(mode))
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-   global mode
-   if request.method == "POST":
-      if request.form["value"] in "True":
-         mode = True
-      else:
-         mode = False
-      return render(mode)
-   else:
-      return render(mode)
+    global mode
+    if request.method == "POST":
+        if request.form["value"] in "True":
+            mode = True
+        else:
+            mode = False
+        return render(mode)
+    else:
+        return render(mode)
 
 
 @app.route("/results", methods=["GET", "POST"])
 def results():
-   if (keyword := request.form["keyword"]):
-      if keyword[0] == "!":
-         return redirect(f"https://duckduckgo.com/&q={keyword}")
-      elif keyword[0] == "?":
-         return redirect(f"https://www.google.com/search?q={keyword[1:]}")
-      else:
-         titles, links, urls, snippets = search(keyword)
-         return render_template("results.haml", mode=int(mode), titles=titles,
-                                links=links, urls=urls, snippets=snippets,
-                                value=keyword, items=len(titles))
-   else:
-      return redirect("/")
+    if keyword := request.form["keyword"]:
+        if keyword[0] == "!":
+            return redirect(f"https://duckduckgo.com/&q={keyword}")
+        elif keyword[0] == "?":
+            return redirect(f"https://www.google.com/search?q={keyword[1:]}")
+        else:
+            titles, links, urls, snippets = search(keyword)
+            return render_template("results.haml", mode=int(mode),
+                                   titles=titles, links=links,
+                                   urls=urls, snippets=snippets,
+                                   value=keyword, items=len(titles))
+    else:
+        return redirect("/")
+
 
 
 if __name__ == "__main__":
-   app.run(port=50000)
+    app.run(port=50000)
--- readme.md
+++ readme.md
@@ -1,11 +1,19 @@
 # Riche
 
+When you search in the search box, Google search results are scraped and displayed. Since the scraping accesses each page to get the title and snippet, it takes time for the results to appear.
+
 ## Requirements
 
-- Run `> choco install sass` to install Sass.
-- Run `pip install -r requirements.txt` to install the dependencies for this application.
+- Python3.8+
+- Python Libraries
+  - `pip install -r requirements.txt`
+- Sass
+  - `choco install sass`
 
 ## Usage
 
-1. Next, run `python app.py` to run the server locally on your machine.
+1. Run `python app.py` to run the server locally on your machine.
 2. Open `localhost:50000` to view it in the browser.
+
+If you prefix your search keyword with `?` at the beginning of a search term will redirect you to Google search results page.  
+`!` You can use DuckDuckGo Bang command to redirect to DuckDuckGo search results page.
